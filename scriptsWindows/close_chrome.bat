dfe9ae7b081cd397bd146e99df15adb1febfdd60
Noars <55838787+Noars@users.noreply.github.com>
2022-09-06 13:45:47 +0200

    Fix scripts

--- scriptsWindows/close_chrome.bat
+++ scriptsWindows/close_chrome.bat
@@ -3,6 +3,7 @@ IF EXIST close161918.txt (
 	del close161918.txt
 	taskkill /IM chrome.exe /F
 	taskkill /IM python.exe /F
-	cd C:\\Program Files (x86)\\InterAACtionBoxAFSR\\lib\\scriptsWindows\\
-	reset_port.bat
+	cd "C:\\Program Files (x86)\\InterAACtionBoxAFSR\\lib\\scriptsWindows"
+	reset_ports.bat
 )
+exit
